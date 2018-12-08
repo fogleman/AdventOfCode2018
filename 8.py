be257d870c44274f7f925be87c23514fed94e6c9
Michael Fogleman <fogleman@gmail.com>
2018-12-08 16:00:55 -0500

    Add day 8

---
+++ 8.py
@@ -0,0 +1,26 @@
+import fileinput
+
+def parse(it):
+    num_children, num_metadata = next(it), next(it)
+    children = [parse(it) for _ in range(num_children)]
+    metadata = [next(it) for _ in range(num_metadata)]
+    return (metadata, children)
+
+root = parse(map(int, list(fileinput.input())[0].split()))
+
+# part 1
+def sum_metadata(node):
+    metadata, children = node
+    return sum(metadata) + sum(sum_metadata(x) for x in children)
+
+print(sum_metadata(root))
+
+# part 2
+def node_value(node):
+    metadata, children = node
+    if children:
+        return sum(node_value(children[i-1]) for i in metadata
+            if 1 <= i <= len(children))
+    return sum(metadata)
+
+print(node_value(root))
