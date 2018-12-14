d554f7e037cb4392749f37ea30f42692665e1333
Michael Fogleman <fogleman@gmail.com>
2018-12-14 14:03:47 -0500

    update 14

--- 14.py
+++ 14.py
@@ -2,29 +2,29 @@ import fileinput
 
 n = int(next(fileinput.input()))
 
-def step(scores, current):
-    s = scores[current[0]] + scores[current[1]]
+def step(scores, i, j):
+    s = scores[i] + scores[j]
     if s >= 10:
         scores.append(1)
     scores.append(s % 10)
-    current[0] = (current[0] + scores[current[0]] + 1) % len(scores)
-    current[1] = (current[1] + scores[current[1]] + 1) % len(scores)
+    i = (i + scores[i] + 1) % len(scores)
+    j = (j + scores[j] + 1) % len(scores)
+    return (i, j)
 
 # part 1
-scores, current = [3, 7], [0, 1]
+scores, i, j = [3, 7], 0, 1
 while len(scores) < n + 10:
-    step(scores, current)
+    i, j = step(scores, i, j)
 print(''.join(map(str, scores[n:n+10])))
 
 # part 2
-scores, current = [3, 7], [0, 1]
+scores, i, j = [3, 7], 0, 1
 digits = list(map(int, str(n)))
-num_digits = len(digits)
 while True:
-    step(scores, current)
-    if scores[-num_digits-1:-1] == digits:
-        print(len(scores) - num_digits - 1)
+    i, j = step(scores, i, j)
+    if scores[-len(digits)-1:-1] == digits:
+        print(len(scores) - len(digits) - 1)
         break
-    if scores[-num_digits:] == digits:
-        print(len(scores) - num_digits)
+    if scores[-len(digits):] == digits:
+        print(len(scores) - len(digits))
         break
